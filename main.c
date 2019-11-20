eceaa98caa7aad9a8ba18ba9de50c19cecadd69b
neo <xiongyu0523@gmail.com>
2019-11-20 11:42:35 +0800

    add rtcore to hlcore msg

--- main.c
+++ main.c
@@ -14,80 +14,69 @@
 #define CFIAR10_HEIGHT  32
 #define CFIAR10_DEPTH   3
 
-static const uint32_t Cifar10ImgSize = CFIAR10_WIDTH * CFIAR10_HEIGHT * CFIAR10_DEPTH;
 static uint8_t Cifar10ImgBuf[CFIAR10_WIDTH * CFIAR10_HEIGHT * CFIAR10_DEPTH];
+static const char* cifar10_label[] = {"Plane", "Car", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck" };
 
-int get_top_prediction(q7_t* predictions)
+static uint8_t _get_top_prediction(q7_t* predictions, uint8_t max)
 {
-	int max_ind = 0;
-	int max_val = -128;
-	for (int i = 0; i < 10; i++) {
+	uint8_t index = 0;
+	int8_t  max_val = -128;
+	for (uint8_t i = 0; i < max; i++) {
 		if (max_val < predictions[i]) {
 			max_val = predictions[i];
-			max_ind = i;
+			index = i;
 		}
 	}
-	return max_ind;
+	return index;
 }
-const char* cifar10_label[] = { "Plane", "Car", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck" };
+
 _Noreturn void RTCoreMain(void)
 {
 	VectorTableInit();
 	CPUFreq_Set(197600000);
 	
 	DebugUARTInit();
-
-	Log_Debug("Exmaple to run NN for Cifar-10\r\n");
-
-#if 1
+	Log_Debug("Exmaple to run NN inference on RTcore for cifar-10 dataset\r\n");
 
 	BufferHeader *outbound, *inbound;
 	uint32_t sharedBufSize = 0;
+
 	if (GetIntercoreBuffers(&outbound, &inbound, &sharedBufSize) == -1) {
 		Log_Debug("ERROR: GetIntercoreBuffers failed\r\n");
 		while (1);
 	}
 
-	Log_Debug("outbound = 0x%X, inbound = 0x%X, sharedBufSize = %d\r\n", outbound, inbound, sharedBufSize);
-
 	const size_t payloadStart = 20;
 	const uint32_t maxInterCoreBufSize = 1024;
-
 	uint32_t piece = 0;
 	uint32_t recvSize = maxInterCoreBufSize + payloadStart;
-	uint8_t  recvBuf[maxInterCoreBufSize + payloadStart];
+	uint8_t recvBuf[maxInterCoreBufSize + payloadStart];
+	q7_t output_data[10];
+	uint8_t top_index;
 
 	while (1) {
 
+		// waiting for incoming data
 		if (DequeueData(outbound, inbound, sharedBufSize, &recvBuf[0], &recvSize) == -1) {
 			continue;
 		}
 
+		// 3072 = 1024 x 3, as the maximum allowed user payload is 1024, we need split into 3 buffer. (HL and RT core must be synced)
 		if (piece < 3) {
 			memcpy(&Cifar10ImgBuf[piece * maxInterCoreBufSize], &recvBuf[payloadStart], maxInterCoreBufSize);
 			piece++;
 		}
 
-		q7_t output_data[10];
-
 		if (piece == 3) {
 			piece = 0;
 
+			// input = 32x32x3 RGB data, output = Possibility of each class
 			run_nn(&Cifar10ImgBuf[0], output_data);
-			int top_ind = get_top_prediction(output_data);
-			Log_Debug("%s\r\n", cifar10_label[top_ind]);
-			//for (int i = 0; i < 10; i++)
-			//{
-			//	Log_Debug("%d: %d\r\n", i, output_data[i]);
-			//}
-			//Log_Debug("start\r\n");
-			//for (uint32_t pos = 0; pos < 3072; pos++) {
-			//	Log_Debug("%d, ", Cifar10ImgBuf[pos]);
-			//}
-			//Log_Debug("\r\n");
+			top_index = _get_top_prediction(output_data, 10);
+
+			// Send the result back to HL core
+			recvBuf[payloadStart] = top_index;
+			EnqueueData(inbound, outbound, sharedBufSize, &recvBuf[0], payloadStart + 1);
 		}
 	}
-
-#endif
-
 }
